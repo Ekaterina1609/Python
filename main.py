58d642b87cd22fc0b306a0aded607ba04c906162
Ekaterina1609 <shimina@bk.ru>
2023-08-01 18:05:37 +0300

    start

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Hello, мир")
-print("Hello, мир2")
+# num =int(input("введите число"))
+# print(num**2)
+
+
+#name = input("Введите имя: ")
+#age = int(input("Введите возраст:"))
+#print(f"Привет! {name}. Тебе {age} лет")
+#pas = 123
+#pas_check=input('Введите пароль')
+#if pas_check==pas:
+#    print("Правильный пароль")
+#else:
+#    print("Неправильный пароль")
+
+time = int(input("Введите время в сек: "))
+hour = (time//3600)
+print(hour)
+minutes = (time-hour*3600)//60
+print(minutes)
+sec=time-hour*3600-minutes*60
+
+print(f"{hour} часов {minutes} минут {sec} секунд")
